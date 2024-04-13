6eca6c725c85de79dadfd763272d46b970a714aa
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-04-13 21:44:31 +0200

    added darkMode

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,9 +1,38 @@
 const hamburger = document.querySelector('.nav-hamburger-li');
 const list = document.querySelector('.header--nav ul');
 const backdrop = document.createElement('div');
+const nightModeButton = document.querySelector('.night-mode')
+
 backdrop.classList.add('backdrop');
 backdrop.style.backgroundColor = "white";
 document.body.appendChild(backdrop);
+
+
+let nightMode = false; // Zmienna śledząca stan trybu nocnego
+
+nightModeButton.addEventListener('click', function () {
+   nightMode = !nightMode; // Zmień stan trybu nocnego po każdym kliknięciu
+
+   if (nightMode) {
+      // Zmiana kolorów na tryb nocny
+      document.documentElement.style.setProperty('--body-color', '#000');
+      document.documentElement.style.setProperty('--color-white', '#000');
+      document.documentElement.style.setProperty('--color-black', '#fff');
+      document.body.style.color = '#fff';
+      document.querySelector('.header').style.backgroundColor = '#000';
+      document.querySelector('.header').style.color = '#fff';
+      document.querySelector('.download-cv-button a').style.color = '#000';
+   } else {
+      // Przywrócenie pierwotnych kolorów
+      document.documentElement.style.setProperty('--body-color', 'rgb(250, 250, 250)');
+      document.documentElement.style.setProperty('--color-white', '#fff');
+      document.documentElement.style.setProperty('--color-black', '#000');
+      document.body.style.color = '#000';
+      document.querySelector('.header').style.backgroundColor = 'initial';
+      document.querySelector('.header').style.color = 'initial';
+   }
+});
+
 
 hamburger.addEventListener('click', function () {
    if (list.style.display === "flex") {
@@ -35,5 +64,4 @@ function resizeHandler() {
 
 
 window.addEventListener('resize', resizeHandler);
-
 window.addEventListener('load', resizeHandler);
