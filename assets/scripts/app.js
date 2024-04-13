114c1608c5ee5beaca731e5983fb0f9ed60ea764
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-04-13 19:42:49 +0200

    finished navigation

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,11 +1,15 @@
 const hamburger = document.querySelector('.nav-hamburger-li');
 const list = document.querySelector('.header--nav ul');
+const backdrop = document.createElement('div');
+backdrop.classList.add('backdrop');
+document.body.appendChild(backdrop);
 
 hamburger.addEventListener('click', function () {
    if (list.style.display === "flex") {
-      list.style.display = "none"
+      list.style.display = "none";
+      backdrop.style.display = "none"; // Ukryj tło, gdy menu jest zamknięte
    } else {
       list.style.display = "flex";
+      backdrop.style.display = "block"; // Pokaż tło, gdy menu jest otwarte
    }
-
 });
