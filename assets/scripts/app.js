70a06c065875efd6672a028ee39c60f4d428f6b6
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-04-13 20:53:28 +0200

    added mobile view

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,14 +2,17 @@ const hamburger = document.querySelector('.nav-hamburger-li');
 const list = document.querySelector('.header--nav ul');
 const backdrop = document.createElement('div');
 backdrop.classList.add('backdrop');
+backdrop.style.backgroundColor = "white"; // Ustaw kolor tła na biały
 document.body.appendChild(backdrop);
 
 hamburger.addEventListener('click', function () {
    if (list.style.display === "flex") {
       list.style.display = "none";
       backdrop.style.display = "none"; // Ukryj tło, gdy menu jest zamknięte
+      document.body.style.overflow = "auto"; // Przywróć overflow do wartości domyślnej
    } else {
       list.style.display = "flex";
       backdrop.style.display = "block"; // Pokaż tło, gdy menu jest otwarte
+      document.body.style.overflow = "hidden"; // Ukryj overflow, gdy menu jest otwarte
    }
 });
