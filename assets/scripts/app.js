e9f27ccbd04920dca801f823fc840e1caed0e439
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-04-14 21:54:51 +0200

    added skills section

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,115 +1,116 @@
-const hamburger = document.querySelector('.nav-hamburger-li');
-const list = document.querySelector('.header--nav ul');
-const backdrop = document.createElement('div');
-const nightModeButton = document.querySelector('.night-mode');
-const nightModeMobile = document.querySelector('.night');
+const hamburger = document.querySelector(".nav-hamburger-li");
+const list = document.querySelector(".header--nav ul");
+const backdrop = document.createElement("div");
+const nightModeButton = document.querySelector(".night-mode");
+const nightModeMobile = document.querySelector(".night");
 
-const nightModeIcon = document.querySelector('.night-mode');
-const mobileNightModeIcon = document.querySelector('.night');
+const nightModeIcon = document.querySelector(".night-mode");
+const mobileNightModeIcon = document.querySelector(".night");
 
-const glass = document.querySelector('.glass');
+const glass = document.querySelector(".glass");
 
-backdrop.classList.add('backdrop');
+backdrop.classList.add("backdrop");
 backdrop.style.backgroundColor = "white";
 document.body.appendChild(backdrop);
 
-
 let nightMode = false;
 
-
 // MOBILE
-nightModeMobile.addEventListener('click', function () {
-   nightMode = !nightMode;
-   if (nightMode) {
-      backdrop.style.backgroundColor = "black";
-      document.body.appendChild(backdrop);
-      // Zmiana kolorów na tryb nocny
-      document.documentElement.style.setProperty('--body-color', '#000');
-      document.documentElement.style.setProperty('--color-white', '#000');
-      document.documentElement.style.setProperty('--color-black', '#fff');
-      document.body.style.color = '#fff';
-      document.querySelector('.header').style.backgroundColor = '#000';
-      document.querySelector('.header').style.color = '#fff';
-      document.querySelector('.download-cv-button a').style.color = '#000';
-      mobileNightModeIcon.classList.remove('uil-moon');
-      mobileNightModeIcon.classList.add('uil-sun');
-      glass.style.boxShadow = '0 8px 32px 0 rgba(255, 255, 255, 0.37)';
-   } else {
-      backdrop.style.backgroundColor = "white";
-      document.body.appendChild(backdrop);
-      // Przywrócenie pierwotnych kolorów
-      document.documentElement.style.setProperty('--body-color', 'rgb(250, 250, 250)');
-      document.documentElement.style.setProperty('--color-white', '#fff');
-      document.documentElement.style.setProperty('--color-black', '#000');
-      document.body.style.color = '#000';
-      document.querySelector('.header').style.backgroundColor = '#fff';
-      document.querySelector('.header').style.color = 'initial';
-      mobileNightModeIcon.classList.remove('uil-sun');
-      mobileNightModeIcon.classList.add('uil-moon');
-      glass.style.boxShadow = '0 8px 32px 0 rgba(0, 0, 0, 0.37)';
-   }
+nightModeMobile.addEventListener("click", function () {
+  nightMode = !nightMode;
+  if (nightMode) {
+    backdrop.style.backgroundColor = "black";
+    document.body.appendChild(backdrop);
+    // Zmiana kolorów na tryb nocny
+    document.documentElement.style.setProperty("--body-color", "#000");
+    document.documentElement.style.setProperty("--color-white", "#000");
+    document.documentElement.style.setProperty("--color-black", "#fff");
+    document.body.style.color = "#fff";
+    document.querySelector(".header").style.backgroundColor = "#000";
+    document.querySelector(".header").style.color = "#fff";
+    document.querySelector(".download-cv-button a").style.color = "#000";
+    mobileNightModeIcon.classList.remove("uil-moon");
+    mobileNightModeIcon.classList.add("uil-sun");
+    glass.style.boxShadow = "0 8px 32px 0 rgba(255, 255, 255, 0.37)";
+  } else {
+    backdrop.style.backgroundColor = "white";
+    document.body.appendChild(backdrop);
+    // Przywrócenie pierwotnych kolorów
+    document.documentElement.style.setProperty(
+      "--body-color",
+      "rgb(250, 250, 250)",
+    );
+    document.documentElement.style.setProperty("--color-white", "#fff");
+    document.documentElement.style.setProperty("--color-black", "#000");
+    document.body.style.color = "#000";
+    document.querySelector(".header").style.backgroundColor = "#fff";
+    document.querySelector(".header").style.color = "initial";
+    mobileNightModeIcon.classList.remove("uil-sun");
+    mobileNightModeIcon.classList.add("uil-moon");
+    glass.style.boxShadow = "0 8px 32px 0 rgba(0, 0, 0, 0.37)";
+  }
 });
-
 
 // DESKTOP
-nightModeButton.addEventListener('click', function () {
-   nightMode = !nightMode;
+nightModeButton.addEventListener("click", function () {
+  nightMode = !nightMode;
 
-   if (nightMode) {
-      // Zmiana kolorów na tryb nocny
-      document.documentElement.style.setProperty('--body-color', '#000');
-      document.documentElement.style.setProperty('--color-white', '#000');
-      document.documentElement.style.setProperty('--color-black', '#fff');
-      document.body.style.color = '#fff';
-      document.querySelector('.header').style.backgroundColor = '#000';
-      document.querySelector('.header').style.color = '#fff';
-      document.querySelector('.download-cv-button a').style.color = '#000';
-      nightModeIcon.classList.remove('uil-moon');
-      nightModeIcon.classList.add('uil-sun');
-      glass.style.boxShadow = '0 8px 32px 0 rgba(255, 255, 255, 0.37)';
-   } else {
-      // Przywrócenie pierwotnych kolorów
-      document.documentElement.style.setProperty('--body-color', 'rgb(250, 250, 250)');
-      document.documentElement.style.setProperty('--color-white', '#fff');
-      document.documentElement.style.setProperty('--color-black', '#000');
-      document.body.style.color = '#000';
-      document.querySelector('.header').style.backgroundColor = 'initial';
-      document.querySelector('.header').style.color = 'initial';
-      nightModeIcon.classList.remove('uil-sun');
-      nightModeIcon.classList.add('uil-moon');
-      glass.style.boxShadow = '0 8px 32px 0 rgba(0, 0, 0, 0.37)';
-   }
+  if (nightMode) {
+    // Zmiana kolorów na tryb nocny
+    document.documentElement.style.setProperty("--body-color", "#000");
+    document.documentElement.style.setProperty("--color-white", "#000");
+    document.documentElement.style.setProperty("--color-black", "#fff");
+    document.body.style.color = "#fff";
+    document.querySelector(".header").style.backgroundColor = "#000";
+    document.querySelector(".header").style.color = "#fff";
+    document.querySelector(".download-cv-button a").style.color = "#000";
+    nightModeIcon.classList.remove("uil-moon");
+    nightModeIcon.classList.add("uil-sun");
+    glass.style.boxShadow = "0 8px 32px 0 rgba(255, 255, 255, 0.37)";
+  } else {
+    // Przywrócenie pierwotnych kolorów
+    document.documentElement.style.setProperty(
+      "--body-color",
+      "rgb(250, 250, 250)",
+    );
+    document.documentElement.style.setProperty("--color-white", "#fff");
+    document.documentElement.style.setProperty("--color-black", "#000");
+    document.body.style.color = "#000";
+    document.querySelector(".header").style.backgroundColor = "#fff";
+    document.querySelector(".header").style.color = "initial";
+    nightModeIcon.classList.remove("uil-sun");
+    nightModeIcon.classList.add("uil-moon");
+    glass.style.boxShadow = "0 8px 32px 0 rgba(0, 0, 0, 0.37)";
+  }
 });
 
-
-hamburger.addEventListener('click', function () {
-   if (list.style.display === "flex") {
-      closeMenu();
-   } else {
-      openMenu();
-   }
+hamburger.addEventListener("click", function () {
+  if (list.style.display === "flex") {
+    closeMenu();
+  } else {
+    openMenu();
+  }
 });
 
 function closeMenu() {
-   list.style.display = "none";
-   backdrop.style.display = "none";
-   document.body.style.overflow = "auto";
+  list.style.display = "none";
+  backdrop.style.display = "none";
+  document.body.style.overflow = "auto";
 }
 
 function openMenu() {
-   list.style.display = "flex";
-   backdrop.style.display = "block";
-   document.body.style.overflow = "hidden";
+  list.style.display = "flex";
+  backdrop.style.display = "block";
+  document.body.style.overflow = "hidden";
 }
 
 function resizeHandler() {
-   if (window.innerWidth >= 1000) {
-      list.style.display = "flex";
-   } else if (!hamburger.classList.contains('active')) {
-      closeMenu();
-   }
+  if (window.innerWidth >= 1000) {
+    list.style.display = "flex";
+  } else if (!hamburger.classList.contains("active")) {
+    closeMenu();
+  }
 }
 
-
-window.addEventListener('resize', resizeHandler);
-window.addEventListener('load', resizeHandler);
+window.addEventListener("resize", resizeHandler);
+window.addEventListener("load", resizeHandler);
